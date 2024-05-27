ac759bec6367dd0bf92a1d8e4286a2e360472803
zeiad <zeyad.khaleid@gmail.com>
2024-05-27 18:16:24 +0300

    update colab 05

--- src/utils/sum.js
+++ src/utils/sum.js
@@ -31,7 +31,7 @@ export async function summarize(bookId, sPg, ePg) {
 
   async function query(data) {
     if (data.error) return { error: data.error };
-    const response = await fetch("https://8616-35-193-95-9.ngrok-free.app/", {
+    const response = await fetch("https://c1b2-34-143-194-186.ngrok-free.app/", {
       headers: { "Content-Type": "application/json" },
       method: "POST",
       body: JSON.stringify({ text: data }),
